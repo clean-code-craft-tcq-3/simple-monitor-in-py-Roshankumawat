52293c4a6697bcc4303a733d62cfc76ab09bf024
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-13 20:32:56 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -2,7 +2,8 @@ def battery_is_ok(temperature, soc, charge_rate):
     return(check_temperature(temperature) and check_soc(soc) and check_charge_rate(charge_rate))
        
 def check_temperature(temperature):
-    check_threshold_limit(0,45,temperature)
+    check_lower_threshold_limit(0,45,temperature)
+    check_upper_threshold_limit(45, temperature)
     if ( 0 >temperature or temperature> 45):
         print_text('Temperature is out of range!')
         return False
@@ -10,7 +11,8 @@ def check_temperature(temperature):
        return True
       
 def check_soc(soc):
-    check_threshold_limit(20, 80, soc)
+    check_lower_threshold_limit(20, 80, soc)
+    check_upper_threshold_limit(80, soc)
     if ( 20 > soc or soc > 80):
         print_text('State of Charge is out of range!')
         return False
@@ -18,7 +20,8 @@ def check_soc(soc):
         return True
         
 def check_charge_rate(charge_rate):
-    check_threshold_limit(0,0.8, charge_rate)
+    check_lower_threshold_limit(0,0.8, charge_rate)
+    check_upper_threshold_limit(0.8, charge_rate)
     if charge_rate >0.8:
         print_text('Charge rate is out of range!')
         return False
@@ -28,11 +31,13 @@ def check_charge_rate(charge_rate):
 def print_text(text):
     print(text)
 
-def check_threshold_limit(lower_limit, upper_limit, value):   
+def check_lower_threshold_limit(lower_limit, upper_limit, value):   
     if((value<=(lower_limit+(upper_limit*5)/100) and value>=lower_limit)):
         print_text('Warning: Approaching discharge') 
-    #elif((value>=(upper_limit-(upper_limit*5)/100) and value<=upper_limit)):
-        #print_text('Warning: Approaching charge-peak')
+
+def check_upper_threshold_limit(upper_limit, value):
+    if((value>=(upper_limit-(upper_limit*5)/100) and value<=upper_limit)):
+        print_text('Warning: Approaching charge-peak')
      
 if __name__ == '__main__':
     assert(check_temperature(1) is True)
