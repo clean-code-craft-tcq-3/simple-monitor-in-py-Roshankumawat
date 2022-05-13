b4969f71364ab81f56805f5c900b16e9f7e1e137
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-13 20:05:15 +0530

    Update check_limits.py
    Early Warning

--- check_limits.py
+++ check_limits.py
@@ -1,7 +1,8 @@
 def battery_is_ok(temperature, soc, charge_rate):
-    return(check_tepmerature(temperature) and check_soc(soc) and check_charge_rate(charge_rate))
+    return(check_temperature(temperature) and check_soc(soc) and check_charge_rate(charge_rate))
        
-def check_tepmerature(temperature):
+def check_temperature(temperature):
+    calculate_tolerance(0,45,temperature)
     if ( 0 >temperature or temperature> 45):
         print_text('Temperature is out of range!')
         return False
@@ -9,6 +10,7 @@ def check_tepmerature(temperature):
        return True
       
 def check_soc(soc):
+    calculate_tolerance(20, 80, soc)
     if ( 20 > soc or soc > 80):
         print_text('State of Charge is out of range!')
         return False
@@ -16,6 +18,7 @@ def check_soc(soc):
         return True
         
 def check_charge_rate(charge_rate):
+    calculate_tolerance(0,0.8, charge_rate)
     if charge_rate >0.8:
         print_text('Charge rate is out of range!')
         return False
@@ -25,11 +28,22 @@ def check_charge_rate(charge_rate):
 def print_text(text):
     print(text)
 
+def calculate_tolerance(lower_limit, upper_limit, value):
+    lower_limit_with_tolerance= lower_limit+((upper_limit*5)/100)
+    upper_limit_with_tolerance= upper_limit-((upper_limit*5)/100)
+    print_warning(lower_limit,lower_limit_with_tolerance, upper_limit, upper_limit_with_tolerance, value)
+ 
+def print_warning(lower_limit,lower_limit_with_tolerance, upper_limit, upper_limit_with_tolerance, value):
+    if((value<=lower_limit_with_tolerance and value>=lower_limit)):
+        print_text('Warning: Approaching discharge') 
+    elif((value>=upper_limit_with_tolerance and value<=upper_limit)):
+        print_text('Warning: Approaching charge-peak')
+     
 if __name__ == '__main__':
-    assert(check_tepmerature(1) is True)
-    assert(check_tepmerature(45)is True)
-    assert(check_tepmerature(46)is False)
-    assert(check_tepmerature(-1)is False)
+    assert(check_temperature(1) is True)
+    assert(check_temperature(45)is True)
+    assert(check_temperature(46)is False)
+    assert(check_temperature(-1)is False)
     assert(check_soc(20)is True)
     assert(check_soc(19)is False)
     assert(check_soc(80)is True)
