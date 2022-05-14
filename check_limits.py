5e53b904c3275f3df38451366495bda51cda5dfc
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-05-14 08:57:38 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -2,42 +2,33 @@ def battery_is_ok(temperature, soc, charge_rate):
     return(check_temperature(temperature) and check_soc(soc) and check_charge_rate(charge_rate))
        
 def check_temperature(temperature):
-    check_lower_threshold_limit(0,45,temperature)
-    check_upper_threshold_limit(45, temperature)
-    if ( 0 >temperature or temperature> 45):
-        print_text('Temperature is out of range!')
-        return False
-    else:
-       return True
+    return check_feature_limit(0,45,temperature, 'Temperature')
       
 def check_soc(soc):
-    check_lower_threshold_limit(20, 80, soc)
-    check_upper_threshold_limit(80, soc)
-    if ( 20 > soc or soc > 80):
-        print_text('State of Charge is out of range!')
-        return False
-    else:
-        return True
+    return check_feature_limit(20,80,soc, 'SOC')
         
 def check_charge_rate(charge_rate):
-    check_lower_threshold_limit(0,0.8, charge_rate)
-    check_upper_threshold_limit(0.8, charge_rate)
-    if charge_rate >0.8:
-        print_text('Charge rate is out of range!')
+    return check_feature_limit(0,0.8,charge_rate, 'Charge_Rate')
+
+def check_feature_limit(lower_limit, upper_limit, feature_value, feature):
+    check_lower_threshold_limit(lower_limit, upper_limit, feature_value)
+    check_upper_threshold_limit( upper_limit, feature_value)
+    if(feature_value<lower_limit or feature_value>upper_limit):
+        print(feature,":",feature_value, "is out of range")
         return False
     else:
         return True
+
+def check_lower_threshold_limit(lower_limit, upper_limit, feature_value):   
+    if((feature_value<=(lower_limit+(upper_limit*5)/100) and feature_value>=lower_limit)):
+        print_text('Warning: Approaching discharge') 
+
+def check_upper_threshold_limit(upper_limit, feature_value):
+    if((feature_value>=(upper_limit-(upper_limit*5)/100) and feature_value<=upper_limit)):
+        print_text('Warning: Approaching charge-peak')
         
 def print_text(text):
     print(text)
-
-def check_lower_threshold_limit(lower_limit, upper_limit, value):   
-    if((value<=(lower_limit+(upper_limit*5)/100) and value>=lower_limit)):
-        print_text('Warning: Approaching discharge') 
-
-def check_upper_threshold_limit(upper_limit, value):
-    if((value>=(upper_limit-(upper_limit*5)/100) and value<=upper_limit)):
-        print_text('Warning: Approaching charge-peak')
      
 if __name__ == '__main__':
     assert(check_temperature(1) is True)
